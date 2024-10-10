b4289c8ef5ce9d399e956b740b3b8da1d628223e
Eunkyo <jeongeunkyo3@gmail.com>
2024-10-11 00:03:18 +0900

    level2-10 QueryDSL

--- src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQueryRepository.java
@@ -1,7 +1,6 @@
 package org.example.expert.domain.todo.repository;
 
 import org.example.expert.domain.todo.dto.response.TodoProjectionDto;
-import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
