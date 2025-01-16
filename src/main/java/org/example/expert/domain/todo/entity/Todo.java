48b85e23eecabc2944d11529fee945886b322143
sy <tndus1886@naver.com>
2025-01-16 18:29:50 +0900

    fix: manager CascadeType.PERSIST 추가

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -23,14 +23,18 @@ public class Todo extends Timestamped {
     private String contents;
     private String weather;
 
+    // 일정 N : 유저 1
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    // 일정 1 : 댓글 N
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    // 일정 1 : 관리자 N
+    // 할 일을 새로 저장할 시, 할 일을 생성한 유저는 담당자로 자동 등록되어야 한다.
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, String weather, User user) {
