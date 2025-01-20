da42b3d92db03a8999e310b43808ba34e6202ff6
임PRO스터\su992 <su9926@naver.com>
2025-01-20 19:58:41 +0900

    [Lv1-2]Feat: nickname 필드 추가 및 User 생성자 수정
    - User 생성자에 nickname 필드 추가

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.user.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.user.enums.UserRole;
@@ -13,35 +14,38 @@ import org.example.expert.domain.user.enums.UserRole;
 @Table(name = "users")
 public class User extends Timestamped {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(unique = true)
-    private String email;
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private UserRole userRole;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private UserRole userRole;
+	private String nickname; // Lv1-2: 컬럼에 nickname 추가
 
-    public User(String email, String password, UserRole userRole) {
-        this.email = email;
-        this.password = password;
-        this.userRole = userRole;
-    }
+	public User(String email, String password, String nickname, UserRole userRole) {
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.userRole = userRole;
+	}
 
-    private User(Long id, String email, UserRole userRole) {
-        this.id = id;
-        this.email = email;
-        this.userRole = userRole;
-    }
+	private User(Long id, String email, UserRole userRole) {
+		this.id = id;
+		this.email = email;
+		this.userRole = userRole;
+	}
 
-    public static User fromAuthUser(AuthUser authUser) {
-        return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
-    }
+	public static User fromAuthUser(AuthUser authUser) {
+		return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
+	}
 
-    public void changePassword(String password) {
-        this.password = password;
-    }
+	public void changePassword(String password) {
+		this.password = password;
+	}
 
-    public void updateRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
+	public void updateRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
 }
