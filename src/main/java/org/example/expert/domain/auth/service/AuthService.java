863c9c795b21b35ee8ec1f8c178628c71ba8fba9
임PRO스터\su992 <su9926@naver.com>
2025-01-20 22:45:05 +0900

    [Lv1-2]Feat: JWT 토큰 생성시 nickname 정보 추가
    - JWT 토큰 생성 로직을 메서드로 추출함

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.config.JwtUtil;
 import org.example.expert.config.PasswordEncoder;
 import org.example.expert.domain.auth.dto.request.SigninRequest;
@@ -20,44 +21,50 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AuthService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtUtil jwtUtil;
 
-    @Transactional
-    public SignupResponse signup(SignupRequest signupRequest) {
+	@Transactional
+	public SignupResponse signup(SignupRequest signupRequest) {
 
-        if (userRepository.existsByEmail(signupRequest.getEmail())) {
-            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
-        }
+		if (userRepository.existsByEmail(signupRequest.getEmail())) {
+			throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+		}
 
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+		String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
 
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+		UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
-        User newUser = new User(
-                signupRequest.getEmail(),
-                encodedPassword,
-                userRole
-        );
-        User savedUser = userRepository.save(newUser);
+		User newUser = new User(
+			signupRequest.getEmail(),
+			encodedPassword,
+			signupRequest.getNickname(), // Lv1-2
+			userRole
+		);
+		User savedUser = userRepository.save(newUser);
 
-        String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
+		String bearerToken = createBearerToken(savedUser);
 
-        return new SignupResponse(bearerToken);
-    }
+		return new SignupResponse(bearerToken);
+	}
 
-    public SigninResponse signin(SigninRequest signinRequest) {
-        User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
-                () -> new InvalidRequestException("가입되지 않은 유저입니다."));
+	public SigninResponse signin(SigninRequest signinRequest) {
+		User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
+			() -> new InvalidRequestException("가입되지 않은 유저입니다."));
 
-        // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
-        if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
-            throw new AuthException("잘못된 비밀번호입니다.");
-        }
+		// 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
+		if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
+			throw new AuthException("잘못된 비밀번호입니다.");
+		}
 
-        String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getUserRole());
+		String bearerToken = createBearerToken(user);
 
-        return new SigninResponse(bearerToken);
-    }
+		return new SigninResponse(bearerToken);
+	}
+
+	// Lv1-2 JWT 토큰에 nickname 정보 추가
+	private String createBearerToken(User user) {
+		return jwtUtil.createToken(user.getId(), user.getEmail(), user.getNickname(), user.getUserRole());
+	}
 }
