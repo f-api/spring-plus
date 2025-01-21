e9f9e6b4995fa767a704633bb0427517455c8b18
임PRO스터\su992 <su9926@naver.com>
2025-01-21 19:28:48 +0900

    [Lv1-5]Feat: 매개변수 weather, startDateTime, endDateTime 추가

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,5 +1,7 @@
 package org.example.expert.domain.todo.service;
 
+import java.time.LocalDateTime;
+
 import lombok.RequiredArgsConstructor;
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
@@ -48,10 +50,11 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    // Lv1-5
+    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findAllByWeatherAndPeriodAtDesc(pageable, weather, startDateTime, endDateTime);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
