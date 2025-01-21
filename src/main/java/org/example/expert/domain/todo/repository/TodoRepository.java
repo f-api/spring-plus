6585c752374c7b6cef2a5897735044056b5469d3
sy <tndus1886@naver.com>
2025-01-21 17:03:38 +0900

    feat: Custom Repository extends 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,17 +1,13 @@
 package org.example.expert.domain.todo.repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository{
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u "
         + "WHERE (:weather IS NULL OR t.weather = :weather )"
@@ -21,8 +17,8 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
         String weather, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable
     );
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+//    @Query("SELECT t FROM Todo t " +
+//            "LEFT JOIN t.user " +
+//            "WHERE t.id = :todoId")
+//    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
