8a86576d3e151a9a0c6e6b8eb3d2967b85622658
임PRO스터\su992 <su9926@naver.com>
2025-01-21 19:30:40 +0900

    [Lv1-5]Feat: 쿼리문에 Where절 추가
    - 날씨 기준 조회 쿼리문 추가
    - 지정한 기간 기준 조회 쿼리문 추가

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    // Lv1-5: 조건을 만족하는 쿼리문 작성
+    @Query("SELECT t FROM Todo t " +
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (t.weather LIKE :weather OR :weather IS NULL) " +
+            "AND (:startDateTime IS NULL OR t.modifiedAt >= :startDateTime)" +
+            "AND (:endDateTime IS NULL OR t.modifiedAt <= :endDateTime)" +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByWeatherAndPeriodAtDesc(Pageable pageable, @Param("weather") String weather,
+       @Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
