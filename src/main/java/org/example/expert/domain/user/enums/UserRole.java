7b7e7ed401d6d77067d6bf40abf95fde162e8cea
sy <tndus1886@naver.com>
2025-01-22 17:48:53 +0900

    fix: ROLE_ 접두사 추가

--- src/main/java/org/example/expert/domain/user/enums/UserRole.java
+++ src/main/java/org/example/expert/domain/user/enums/UserRole.java
@@ -1,16 +1,23 @@
 package org.example.expert.domain.user.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 
 import java.util.Arrays;
 
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    ADMIN, USER;
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private final String role;
 
     public static UserRole of(String role) {
         return Arrays.stream(UserRole.values())
                 .filter(r -> r.name().equalsIgnoreCase(role))
                 .findFirst()
-                .orElseThrow(() -> new InvalidRequestException("유효하지 않은 UerRole"));
+                .orElseThrow(() -> new InvalidRequestException("유효하지 않은 UserRole"));
     }
 }
