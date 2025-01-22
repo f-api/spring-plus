b63e5ad58dc211251c8405cf8c4108f3a1bf22ea
임PRO스터\su992 <su9926@naver.com>
2025-01-22 14:01:40 +0900

    [Lv2-7]Feat: fatch join을 활용해 N+1 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // Lv2-7 N+1 문제 해결
+    @Query("SELECT c FROM Comment c JOIN fetch c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
