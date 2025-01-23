500a5fda8b703716e0ffee4aab5f46e2697df2c4
임PRO스터\su992 <su9926@naver.com>
2025-01-23 12:22:28 +0900

    [Lv2-9]Feat: UserRole에 role 필드 추가
    - Spring Security의 ROLE_ 접두사와 호환을 위해 role 필드 추가

--- src/main/java/org/example/expert/domain/user/enums/UserRole.java
+++ src/main/java/org/example/expert/domain/user/enums/UserRole.java
@@ -4,8 +4,17 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 
 import java.util.Arrays;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
-    ADMIN, USER;
+    ADMIN("ROLE_ADMIN", "관리자 권한"),
+    USER("ROLE_USER", "사용자 권한");
+
+    private final String role;
+    private final String description;
 
     public static UserRole of(String role) {
         return Arrays.stream(UserRole.values())
