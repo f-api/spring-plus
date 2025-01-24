fe4b9e51782305afcb44163f2866bc8a699fb8a2
임PRO스터\su992 <su9926@naver.com>
2025-01-24 16:49:25 +0900

    [Lv3-10]Feat: 일정 검색 API 추가

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.todo.controller;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import jakarta.validation.Valid;
@@ -10,8 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.request.TodoSearchRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -36,18 +39,14 @@ public class TodoController {
 	public ResponseEntity<Page<TodoResponse>> getTodos(
 		@RequestParam(defaultValue = "1") int page,
 		@RequestParam(defaultValue = "10") int size,
-        // Lv1-5
+		// Lv1-5
 		@RequestParam(required = false) String weather,
 		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
 		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
 	) {
-        // Lv1-5
-		LocalDateTime startDateTime = Optional.ofNullable(startDate)
-                                              .map(LocalDate::atStartOfDay)
-                                              .orElse(null);
-		LocalDateTime endDateTime = Optional.ofNullable(endDate)
-                                            .map(date -> date.atTime(23,59,59))
-                                            .orElse(null);
+		// Lv1-5
+		LocalDateTime startDateTime = getStartDateTime(startDate);
+		LocalDateTime endDateTime = getEndDateTime(endDate);
 
 		return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDateTime, endDateTime));
 	}
@@ -56,4 +55,31 @@ public class TodoController {
 	public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
 		return ResponseEntity.ok(todoService.getTodo(todoId));
 	}
+
+	// Lv3-10: 일정 검색 API
+	@GetMapping("/todos/search")
+	public ResponseEntity<List<TodoSearchResponse>> searchTodo(
+		@ModelAttribute TodoSearchRequest todoSearchRequest,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+		@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
+	) {
+		LocalDateTime startDateTime = getStartDateTime(startDate);
+		LocalDateTime endDateTime = getEndDateTime(endDate);
+
+		return ResponseEntity.ok(todoService.searchTodo(todoSearchRequest, startDateTime, endDateTime));
+	}
+
+	/* 날짜 변환 메서드 */
+	private static LocalDateTime getStartDateTime(LocalDate startDate) {
+		return Optional.ofNullable(startDate)
+			.map(LocalDate::atStartOfDay)
+			.orElse(null);
+	}
+
+	private static LocalDateTime getEndDateTime(LocalDate endDate) {
+		return Optional.ofNullable(endDate)
+			.map(date -> date.atTime(23, 59, 59))
+			.orElse(null);
+	}
+
 }
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -1,14 +1,18 @@
 package org.example.expert.domain.todo.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.client.WeatherClient;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.request.TodoSearchRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -24,63 +28,72 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class TodoService {
 
-    private final TodoRepository todoRepository;
-    private final WeatherClient weatherClient;
+	private final TodoRepository todoRepository;
+	private final WeatherClient weatherClient;
 
-    @Transactional
-    public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
-        User user = User.fromAuthUser(authUser);
+	@Transactional
+	public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
+		User user = User.fromAuthUser(authUser);
 
-        String weather = weatherClient.getTodayWeather();
+		String weather = weatherClient.getTodayWeather();
 
-        Todo newTodo = new Todo(
-                todoSaveRequest.getTitle(),
-                todoSaveRequest.getContents(),
-                weather,
-                user
-        );
-        Todo savedTodo = todoRepository.save(newTodo);
+		Todo newTodo = new Todo(
+			todoSaveRequest.getTitle(),
+			todoSaveRequest.getContents(),
+			weather,
+			user
+		);
+		Todo savedTodo = todoRepository.save(newTodo);
 
-        return new TodoSaveResponse(
-                savedTodo.getId(),
-                savedTodo.getTitle(),
-                savedTodo.getContents(),
-                weather,
-                new UserResponse(user.getId(), user.getEmail())
-        );
-    }
+		return new TodoSaveResponse(
+			savedTodo.getId(),
+			savedTodo.getTitle(),
+			savedTodo.getContents(),
+			weather,
+			new UserResponse(user.getId(), user.getEmail())
+		);
+	}
 
-    // Lv1-5
-    public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+	// Lv1-5
+	public Page<TodoResponse> getTodos(int page, int size, String weather, LocalDateTime startDateTime,
+		LocalDateTime endDateTime) {
+		Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByWeatherAndPeriodAtDesc(pageable, weather, startDateTime, endDateTime);
+		Page<Todo> todos = todoRepository.findAllByWeatherAndPeriodAtDesc(pageable, weather, startDateTime,
+			endDateTime);
 
-        return todos.map(todo -> new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
-        ));
-    }
+		return todos.map(todo -> new TodoResponse(
+			todo.getId(),
+			todo.getTitle(),
+			todo.getContents(),
+			todo.getWeather(),
+			new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
+			todo.getCreatedAt(),
+			todo.getModifiedAt()
+		));
+	}
 
-    public TodoResponse getTodo(long todoId) {
-        Todo todo = todoRepository.findByIdWithUser(todoId)
-                .orElseThrow(() -> new InvalidRequestException("Todo not found"));
+	public TodoResponse getTodo(long todoId) {
+		Todo todo = todoRepository.findByIdWithUser(todoId)
+			.orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        User user = todo.getUser();
+		User user = todo.getUser();
 
-        return new TodoResponse(
-                todo.getId(),
-                todo.getTitle(),
-                todo.getContents(),
-                todo.getWeather(),
-                new UserResponse(user.getId(), user.getEmail()),
-                todo.getCreatedAt(),
-                todo.getModifiedAt()
-        );
-    }
+		return new TodoResponse(
+			todo.getId(),
+			todo.getTitle(),
+			todo.getContents(),
+			todo.getWeather(),
+			new UserResponse(user.getId(), user.getEmail()),
+			todo.getCreatedAt(),
+			todo.getModifiedAt()
+		);
+	}
+
+	// Lv3-10
+	public List<TodoSearchResponse> searchTodo(TodoSearchRequest todoSearchRequest, LocalDateTime startDateTime,
+		LocalDateTime endDateTime) {
+
+		return todoRepository.searchTodos(todoSearchRequest, startDateTime, endDateTime);
+	}
 }
