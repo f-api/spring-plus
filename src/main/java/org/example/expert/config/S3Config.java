1c104b19349eb348980d5f49eea94de648910e72
yeongbinim <yeim.dev@gmail.com>
2025-01-27 13:48:32 +0900

    lv3-12: S3 Image 업로더 구현

--- src/main/java/org/example/expert/config/S3Config.java
+++ src/main/java/org/example/expert/config/S3Config.java
@@ -1,4 +1,31 @@
 package org.example.expert.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
 public class S3Config {
+
+    @Value("${image.s3.access-key}")
+    private String accessKey;
+
+    @Value("${image.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${image.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .build();
+    }
 }
--- src/main/java/org/example/expert/domain/common/util/image/S3ImageUtil.java
+++ src/main/java/org/example/expert/domain/common/util/image/S3ImageUtil.java
@@ -1,4 +1,76 @@
-package org.example.expert.domain.common.util;
+package org.example.expert.domain.common.util.image;
 
-public class S3ImageUtil {
+import static org.example.expert.domain.common.exception.ExceptionType.SERVER_IMAGE_DELETE_FAILED;
+import static org.example.expert.domain.common.exception.ExceptionType.SERVER_IMAGE_UPLOAD_FAILED;
+
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.common.exception.CustomException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Primary
+@Component
+@RequiredArgsConstructor
+public class S3ImageUtil implements ImageUtil {
+
+    private final S3Client s3Client;
+
+    @Value("${image.s3.bucket}")
+    private String bucketName;
+
+    @Value("${image.s3.endpoint}")
+    private String endpoint;
+
+    @Override
+    public String upload(MultipartFile image) {
+        if (image == null || image.isEmpty()) {
+            return null;
+        }
+
+        String originalFileName = image.getOriginalFilename();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String filename = UUID.randomUUID() + extension;
+
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key("public/" + filename)
+                .contentType(image.getContentType())
+                .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(image.getBytes()));
+
+            System.out.println(getImageUrl(filename));
+            return filename;
+        } catch (IOException e) {
+            throw new CustomException(SERVER_IMAGE_UPLOAD_FAILED);
+        }
+    }
+
+    @Override
+    public void delete(String filename) {
+        try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key("public/" + filename)
+                .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new CustomException(SERVER_IMAGE_DELETE_FAILED);
+        }
+    }
+
+    @Override
+    public String getImageUrl(String filename) {
+        return endpoint + "/" + filename;
+    }
 }
