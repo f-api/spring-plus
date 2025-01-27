8928bceb619ea3898dc49b605aef150a596cd0ce
yeongbinim <yeim.dev@gmail.com>
2025-01-27 11:10:16 +0900

    lv1-7: `getComments()`에서 `@EntityGraph`를 통해 N+1 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package org.example.expert.domain.comment.repository;
 
 import java.util.List;
 import org.example.expert.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @EntityGraph(attributePaths = {"user"})
     List<Comment> findAllByTodoId(Long todoId);
 }
